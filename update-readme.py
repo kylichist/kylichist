de50e7c99971d717fae00ead27f7817bb5851c66
kylichist <pimenovpetya@gmail.com>
2021-09-12 16:56:36 +0300

    Add git commit -a option for update-readme.py

--- update-readme.py
+++ update-readme.py
@@ -4,5 +4,5 @@ import subprocess
 
 demotivator = Demotivator('его идеи', 'будут актуальны всегда')
 demotivator.create('https://github.com/kylichist.png?size=460', url=True, RESULT_FILENAME='main_pic.png', delete_file=True, arrange=True)
-subprocess.run(['git', 'commit', '-m', 'Update README.md'])
+subprocess.run(['git', 'commit', '-a', '-m', 'Update README.md'])
 subprocess.run(['git', 'push'])
