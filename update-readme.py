77d1264be89743f84e7be5f2b5bb9a6c9b25c677
Petr Pimenov <pimenovpetya@gmail.com>
2025-04-13 11:22:52 +0300

    Обновлен README.md, удалены старые файлы и картинки

--- update-readme.py
+++
@@ -1,8 +0,0 @@
-from simpledemotivators import Demotivator
-import subprocess
-
-
-demotivator = Demotivator('его идеи', 'будут актуальны всегда')
-demotivator.create('https://github.com/kylichist.png?size=460', url=True, RESULT_FILENAME='main_pic.png', delete_file=True, arrange=True)
-subprocess.run(['git', 'commit', '-a', '-m', 'Update README.md'])
-subprocess.run(['git', 'push'])
